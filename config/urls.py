7aed490cb5948babcac7d0289a958f218859a044
saminmahmud <samin.mahmud18@gmail.com>
2026-06-07 20:50:08 +0600

    add another url in config

--- config/urls.py
+++ config/urls.py
@@ -31,6 +31,5 @@ handler403 = 'config.exceptions.handler403'
 handler404 = 'config.exceptions.handler404'
 handler500 = 'config.exceptions.handler500'
 
-# Only used in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
