4baf8dc85c8aa88056c5fb22247b80beba2afa0a
saminmahmud <samin.mahmud18@gmail.com>
2026-06-16 13:42:49 +0600

    add phone field in admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -7,5 +7,5 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ('username', 'email', 'role', 'is_staff')
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('image', 'role')}),
+        (None, {'fields': ('image', 'role', 'phone')}),
     )
